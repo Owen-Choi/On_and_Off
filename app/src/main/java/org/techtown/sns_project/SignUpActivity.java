b31520cdbf4fdd75bb5517ade95c7a122c929d9b
Owen-Choi <oldstyle482@gmail.com>
2022-01-24 20:47:06 +0900

    Firebase 로그인, 인증 기능

--- app/src/main/java/org/techtown/sns_project/SignUpActivity.java
+++ app/src/main/java/org/techtown/sns_project/SignUpActivity.java
@@ -1,20 +1,61 @@
 package org.techtown.sns_project;
 
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "SignUpActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.RegisterButton).setOnClickListener(onClickListener);
     }
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null) {
+            currentUser.reload();
+        }
+    }
+
+    private void SignUp() {
+        // EditText로 형 변환해준 이유는 일반 View는 getText 메서드를 사용할 수 없어서이다.
+        String email = ((EditText)findViewById(R.id.EmailEditText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.PasswordEditText)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // 성공했을 때 UI 로직
+                        }
+                        else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            // Toast 일단 생략, 실패했을 때 UI로직
+                        }
+                    }
+                });
+    }
+
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -22,7 +63,7 @@ public class SignUpActivity extends AppCompatActivity {
             switch (view.getId()) {
                 // id가 RegisterButton에서 받아온 아이디라면 :
                 case R.id.RegisterButton:
-                    Log.e("click", "click");
+                    SignUp();
                     break;
             }
         }
