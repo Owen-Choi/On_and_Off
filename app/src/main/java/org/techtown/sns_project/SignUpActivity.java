dcf6d958357776738c7d4b789bdeddf6a7acdc88
Owen-Choi <oldstyle482@gmail.com>
2022-01-24 15:58:55 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/sns_project/SignUpActivity.java
@@ -0,0 +1,30 @@
+package org.techtown.sns_project;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        findViewById(R.id.RegisterButton).setOnClickListener(onClickListener);
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // view에서 id를 받아오는데
+            switch (view.getId()) {
+                // id가 RegisterButton에서 받아온 아이디라면 :
+                case R.id.RegisterButton:
+                    Log.e("click", "click");
+                    break;
+            }
+        }
+    };
+}
