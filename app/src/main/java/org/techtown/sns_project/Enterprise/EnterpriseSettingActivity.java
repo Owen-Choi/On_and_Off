4695e62dbdf86b101d4e112b252e2e274eaee2fc
Owen-Choi <oldstyle482@gmail.com>
2022-02-17 16:17:43 +0900

    일반 회원 환경설정 구현2

--- app/src/main/java/org/techtown/sns_project/Enterprise/EnterpriseSettingActivity.java
+++ app/src/main/java/org/techtown/sns_project/Enterprise/EnterpriseSettingActivity.java
@@ -4,26 +4,38 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import org.techtown.sns_project.R;
+import org.techtown.sns_project.SettingsFragment;
 
 public class EnterpriseSettingActivity extends AppCompatActivity {
+    // 왜 fragment 창이 안뜨는거지??
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.EnterpriseAlarmButton).setOnClickListener(onClickListener);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment()).commit();
+
+        ActionBar menu = getSupportActionBar();
+        menu.setDisplayShowHomeEnabled(true);
+        menu.setDisplayHomeAsUpEnabled(true);
+
     }
 
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.EnterpriseAlarmButton:
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
-            }
-        }
-    };
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+
 }
--- app/src/main/java/org/techtown/sns_project/SettingsFragment.java
+++ app/src/main/java/org/techtown/sns_project/SettingsFragment.java
@@ -1,13 +1,35 @@
 package org.techtown.sns_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.google.firebase.firestore.core.View;
+
+import org.techtown.sns_project.Normal.NormalMainActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    public SettingsFragment() {
+    }
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+          setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        Preference mypref = (Preference)findPreference("contact_preference");
+        mypref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                startActivity(new Intent(getContext(), NormalMainActivity.class));
+                return true;
+            }
+        });
     }
+
 }
