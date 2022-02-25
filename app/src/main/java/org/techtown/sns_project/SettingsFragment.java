f4ef89693532ae5312a278c45c8ef37d01fc5246
Owen-Choi <oldstyle482@gmail.com>
2022-02-25 19:26:17 +0900

    설정 구성4

--- app/src/main/java/org/techtown/sns_project/SettingsFragment.java
+++ app/src/main/java/org/techtown/sns_project/SettingsFragment.java
@@ -1,6 +1,8 @@
 package org.techtown.sns_project;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +11,9 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.techtown.sns_project.Enterprise.Setting.EnterpriseMemberInfo;
 import org.techtown.sns_project.Enterprise.Setting.EnterpriseSettingActivity;
@@ -28,6 +33,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         i.setData(Uri.parse("https://github.com/Owen-Choi/ToyProject_SNS"));
         Preference mypref = (Preference)findPreference("contact_preference");
         Preference memberInfo = (Preference)findPreference("memberInfo");
+        Preference withdrawal = (Preference)findPreference("withdrawal");
 //        SeekBarPreference sp = findPreference("volume_notifications");
 //        sp.getValue();
         mypref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
@@ -44,6 +50,33 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 StartActivity(NormalMemberInfo.class);
+                return true;
+            }
+        });
+
+        withdrawal.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("정말 탈퇴하시겠습니까?");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                        firebaseAuth.getCurrentUser().delete();
+                        // 추가적으로 db의 정보들도 삭제해야함.
+                        builder.setMessage("정상적으로 탈퇴되었습니다.");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // 이 부분은 약간의 수정여지가 있다.
+                                System.exit(0);
+                            }
+                        });
+                        builder.show();
+                    }
+                });
+                builder.show();
                 return true;
             }
         });
