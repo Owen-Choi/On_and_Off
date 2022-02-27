005af0ab3052636ca9e7d699dda948ba7b5492c8
Owen-Choi <oldstyle482@gmail.com>
2022-02-27 17:32:26 +0900

    일반 유저 홈화면 프래그먼트 전환식으로 변경

--- app/src/main/java/org/techtown/sns_project/Normal/NormalMainActivity.java
+++ app/src/main/java/org/techtown/sns_project/Normal/NormalMainActivity.java
@@ -2,6 +2,7 @@ package org.techtown.sns_project.Normal;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -19,31 +21,78 @@ import org.techtown.sns_project.Normal.Setting.NormalSettingActivity;
 import org.techtown.sns_project.R;
 import org.techtown.sns_project.SignUpActivity;
 import org.techtown.sns_project.cameraexample.ScanQR;
+import org.techtown.sns_project.fragment.BoardFragment;
+import org.techtown.sns_project.fragment.HomeFragment;
+import org.techtown.sns_project.fragment.ProfileFragment;
+import org.techtown.sns_project.fragment.QRFragment;
+import org.techtown.sns_project.fragment.SomethingFragment;
 
 public class NormalMainActivity extends AppCompatActivity {
+    Fragment Board_Fragment;
+    Fragment Home_Fragment;
+    Fragment Profile_Fragment;
+    Fragment QR_Fragment;
+    Fragment Something_Fragment;
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal_main);
-        findViewById(R.id.NormalQRScanButton).setOnClickListener(onClickListener);
+        //findViewById(R.id.NormalQRScanButton).setOnClickListener(onClickListener);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if(user == null) {
             StartActivity(SignUpActivity.class);
         }
+
+        Board_Fragment = new BoardFragment();
+        Home_Fragment = new HomeFragment();
+        Profile_Fragment = new ProfileFragment();
+        QR_Fragment = new QRFragment();
+        Something_Fragment = new SomethingFragment();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,Home_Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.nav_board:
+                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,Board_Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.nav_profile:
+                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,Profile_Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.nav_qr:
+                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,QR_Fragment).commitAllowingStateLoss();
+                        return true;
+                    case R.id.nav_something:
+                        getSupportFragmentManager().beginTransaction() .replace(R.id.main_layout,Something_Fragment).commitAllowingStateLoss();
+                        return true;
+                }
+                return true;
+            }
+        });
+
+
     }
     private void StartActivity(Class c) {
         Intent intent = new Intent(this, c);
         startActivity(intent);
     }
-    View.OnClickListener onClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch(view.getId()) {
-                case R.id.NormalQRScanButton:
-                    StartActivity(ScanQR.class);
-            }
-        }
-    };
+//    View.OnClickListener onClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            switch(view.getId()) {
+//                case R.id.NormalQRScanButton:
+//                    StartActivity(ScanQR.class);
+//            }
+//        }
+//    };
     // 메뉴 코드
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/org/techtown/sns_project/fragment/ProfileFragment.java
@@ -0,0 +1,31 @@
+package org.techtown.sns_project.fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import androidx.fragment.app.Fragment;
+
+import org.techtown.sns_project.R;
+
+public class ProfileFragment extends Fragment {
+    private View view;
+
+    private String TAG = "프래그먼트";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        view = inflater.inflate(R.layout.profile_fragment, container, false);
+
+
+        return view;
+    }
+
+}
