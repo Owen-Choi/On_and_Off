0e92ef71c7ec55f7a3e178ab55251c8f31ae0107
lib26 <dladlsqja@naver.com>
2022-03-25 06:33:09 +0900

    storage 이용한 프사 저장 및 변경

--- app/src/main/java/org/techtown/sns_project/fragment/ProfileFragment.java
+++ app/src/main/java/org/techtown/sns_project/fragment/ProfileFragment.java
@@ -2,12 +2,14 @@ package org.techtown.sns_project.fragment;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,11 +23,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
@@ -34,7 +46,10 @@ import org.techtown.sns_project.Closet.Closet_Parser;
 import org.techtown.sns_project.R;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,10 +69,10 @@ public class ProfileFragment extends Fragment {
     String userNick;
 
     //프사
+    CircleImageView imageView;
+    private Uri photoUri;
     private Boolean isPermission = true;
     private static final int PICK_FROM_ALBUM = 1;
-    private File tempFile;
-    private String profile;
 
     Map<String, Object> pro = new HashMap<>();
 
@@ -88,24 +103,9 @@ public class ProfileFragment extends Fragment {
                         System.out.println(userNick);
                     }
                 });
-/*
 
-        //기존 프사 정보가 파베 필드에 있다면 자동 설정해주기(=users 필드에 저장되어있는 uri 절대값 가져오기).
-        //근데 파베이 프사 정보가 없으면(신규 가입자 처럼) 마페 들어갈 때 프사 정보를 못 불러와서 오류나서 전체 주석해놓음. 일단 프사 정보를 필드에서 storage로 바꾸는 작업 해야할듯
-        db.collection("users").document(firebaseUser.getUid()).get().addOnCompleteListener(
-                task -> {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        profile = document.getData().get("myprofile").toString();
-                        tempFile = new File(profile);
-                        setImage();
-
-                        System.out.println(userNick);
-                    }else {
-                        //파베에 정보 없으면 기본이미지로 프사 설정하도록 짜봤는데도 오류남..
-                    }
-                });
-*/
+        //storage에서 프사 가져오기
+        getFireBaseProfileImage(firebaseUser.getUid());
 
         //프사 클릭시 설정
         view.findViewById(R.id.circle_img).setOnClickListener(new View.OnClickListener() {
@@ -113,79 +113,13 @@ public class ProfileFragment extends Fragment {
             public void onClick(View view) {
                 //프사 클릭시 권한 받기
                 tedPermission();
+                //갤러리 ㄱㄱ
                 if(isPermission) goToAlbum();
                 else Toast.makeText(view.getContext(), getResources().getString(R.string.permission_2), Toast.LENGTH_LONG).show();
             }
         });
         return view;
     }
-
-
-    //프사
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        //갤러리에서 사진 가져오는 거 취소했을 때 오류 안나게 해주는 코드
-        if (resultCode != Activity.RESULT_OK) {
-            Toast.makeText(getActivity(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
-
-            if(tempFile != null) {
-                if (tempFile.exists()) {
-                    if (tempFile.delete()) {
-                        Log.e(TAG, tempFile.getAbsolutePath() + " 삭제 성공");
-                        tempFile = null;
-                    }
-                }
-            }
-
-            return;
-        }
-
-        //갤러리에서 사진을 잘 가져왔을 때
-        if (requestCode == PICK_FROM_ALBUM) {
-
-            Uri photoUri = data.getData();
-            Log.d(TAG, "PICK_FROM_ALBUM photoUri : " + photoUri);
-
-            Cursor cursor = null;
-
-            try {
-
-                /*
-                 *  Uri 스키마를
-                 *  content:/// 에서 file:/// 로  변경한다.
-                 */
-                String[] proj = { MediaStore.Images.Media.DATA };
-
-                assert photoUri != null;
-                cursor = getActivity().getContentResolver().query(photoUri, proj, null, null, null);
-
-                assert cursor != null;
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
-                cursor.moveToFirst();
-
-                //갤러리에서 가져온 사진의 절대 경로를 파베 users 필드안에 값을 넣는다
-                this.profile=cursor.getString(column_index);
-                pro.put("myprofile", cursor.getString(column_index));
-                db.collection("users").document(firebaseUser.getUid())
-                        .set(pro, SetOptions.merge());
-                //
-                tempFile = new File(cursor.getString(column_index));
-
-                Log.d(TAG, "tempFile Uri : " + Uri.fromFile(tempFile));
-
-            } finally {
-                if (cursor != null) {
-                    cursor.close();
-                }
-            }
-
-            setImage();
-
-        }
-    }
-
     /**
      *  앨범에서 이미지 가져오기
      */
@@ -196,26 +130,121 @@ public class ProfileFragment extends Fragment {
         startActivityForResult(intent, PICK_FROM_ALBUM);
     }
 
-    /**
-     *  tempFile 을 bitmap 으로 변환 후 ImageView 에 설정한다.
-     */
-    private void setImage() {
+    //프사
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        CircleImageView imageView = view.findViewById(R.id.circle_img);
+        //갤러리에서 사진 가져오는 거 취소했을 때 오류 안나게 해주는 코드
+        if (resultCode != Activity.RESULT_OK) {
+            Toast.makeText(getActivity(), "취소 되었습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        Bitmap originalBm = BitmapFactory.decodeFile(tempFile.getAbsolutePath(), options);
-        Log.d(TAG, "setImage : " + tempFile.getAbsolutePath());
+        //갤러리에서 사진을 잘 가져왔을 때
+        if (requestCode == PICK_FROM_ALBUM) {
 
-        imageView.setImageBitmap(originalBm);
+            photoUri=data.getData();
+            uploadFile();
+/*
 
-        /**
-         *  tempFile 사용 후 null 처리를 해줘야 합니다.
-         *  (resultCode != RESULT_OK) 일 때 tempFile 을 삭제하기 때문에
-         *  기존에 데이터가 남아 있게 되면 원치 않은 삭제가 이뤄집니다.
-         */
-        tempFile = null;
+            try {
+                //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
 
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(view.getContext().getContentResolver(), photoUri);
+                imageView.setImageBitmap(bitmap);
+
+                // 업로드 실행
+                uploadFile();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+*/
+
+        }
+    }
+
+    private void uploadFile() {
+        //업로드할 uri가 있으면 수행
+        if (photoUri != null) {
+            //업로드 진행 Dialog 보이기
+            final ProgressDialog progressDialog = new ProgressDialog(view.getContext());
+            progressDialog.setTitle("파베 업로드중...");
+            progressDialog.show();
+
+            //storage
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            //user id
+            String filename_GetUid = firebaseUser.getUid();
+
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            StorageReference storageRef = storage.getReferenceFromUrl("gs://toyproject-sns.appspot.com/").child("profile_images/" + filename_GetUid);
+            //올라가거라...
+            storageRef.putFile(photoUri)
+                    //성공시
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //이미지 설정
+                            getFireBaseProfileImage(firebaseUser.getUid());
+                            progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                            Toast.makeText(view.getContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //실패시
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(view.getContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //진행중
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
+                            double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            //dialog에 진행률을 퍼센트로 출력해 준다
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(view.getContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void getFireBaseProfileImage(String filename_GetUid){
+        //우선 디렉토리 파일 하나 만든다.
+        File file = view.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES + "/profile_img");//이미지를 저장할 수 있는 디렉토리
+        //구분할 수 있게 /toolbar_image 폴더에 넣어준다.
+        //이 파일안에 저 디렉토리가 있는지 확인
+        if(!file.isDirectory()){ //디렉토리가 없으면,
+            file.mkdir(); //디렉토리를 만든다.
+        }
+        downloadImg(filename_GetUid); //이미지 다운로드해서 가져오기 메서드
+    }
+
+    /**이미지 다운로드해서 가져오기 메서드 */
+    private void downloadImg(String filename_GetUid){
+        FirebaseStorage storage = FirebaseStorage.getInstance(); //스토리지 인스턴스를 만들고,
+        //다운로드는 주소를 넣는다.
+        StorageReference storageRef = storage.getReference(); //스토리지를 참조한다
+        storageRef.child("profile_images/"+filename_GetUid).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //성공시
+                imageView = view.findViewById(R.id.circle_img);
+                Glide.with(view.getContext()).load(uri).into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //실패시
+                imageView = view.findViewById(R.id.circle_img);
+                imageView.setImageResource(R.drawable.ic_baseline_android_24);
+            }
+        });
     }
 
     /**
