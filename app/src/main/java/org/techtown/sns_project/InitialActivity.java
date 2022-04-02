a69c3c1ef2a6b8ad958a8f0b0b94ed508b5636f6
Owen-Choi <oldstyle482@gmail.com>
2022-04-03 01:52:15 +0900

    자동로그인 오류 수정

--- app/src/main/java/org/techtown/sns_project/InitialActivity.java
+++ app/src/main/java/org/techtown/sns_project/InitialActivity.java
@@ -28,12 +28,9 @@ public class InitialActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user == null) {
-            StartActivity(CommonSignInActivity.class);
-        }
-        else{
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null){
             // 로그인이 되어있다면 회원 정보가 등록됐는지 본다.
             String[] temp = {"users", "enterprises"};
             FirebaseFirestore fb = FirebaseFirestore.getInstance();
@@ -45,16 +42,15 @@ public class InitialActivity extends AppCompatActivity {
                         if(task.isSuccessful()) {
                             DocumentSnapshot document = task.getResult();
                             if(document != null) {
-                                if (document.exists()) {
                                     if (tempPath.equals("users"))
                                         StartActivity(NormalMainActivity.class);
                                     else if (tempPath.equals("enterprises"))
                                         StartActivity(EnterpriseMainActivity.class);
-                                }
                                 else {
                                     // 유저는 있는데 db가 없는 상황이 있다.
                                     // 회원탈퇴 기능이 추가되면서 간간히 발생한다.
                                     // 그런 상황을 위한 디펜시브 코드이다.
+                                    Log.e("Unknown Error", "onComplete: login error");
                                     FirebaseAuth.getInstance().signOut();
                                     StartActivity(CommonSignInActivity.class);
                                 }
@@ -69,6 +65,8 @@ public class InitialActivity extends AppCompatActivity {
                 });
             }
         }
+        else
+            StartActivity(CommonSignInActivity.class);
     }
 
     private void StartActivity(Class c) {
