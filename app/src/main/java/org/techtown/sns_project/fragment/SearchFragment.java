7eb9e00cab052466bcb2f9e23659c928c88c3168
dong <idh1007@naver.com>
2022-05-23 16:11:16 +0900

    검색 시 array 중첩 문제 해결 및 QR 찍었을 때 count++ , 제품 정보 보여주는 화면에서 해시태그 제거

--- app/src/main/java/org/techtown/sns_project/fragment/SearchFragment.java
+++ app/src/main/java/org/techtown/sns_project/fragment/SearchFragment.java
@@ -38,7 +38,7 @@ public class SearchFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onCreateView");
         view = inflater.inflate(R.layout.search_fragment, container, false);
-
+        arraylist.clear();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collectionGroup("brand").get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
